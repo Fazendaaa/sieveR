0a16c08ff232bb5aa21d236cc655f2baa10c49f4
Fazendaaa <lucas.carotta@outlook.com>
2021-12-16 17:56:17 -0300

    Adding CORS to server.

--- inst/sieve.R
+++ inst/sieve.R
@@ -1,3 +1,11 @@
+# https://www.rplumber.io/articles/security.html#cross-origin-resource-sharing-cors
+#* @filter cors
+cors <- function(res) {
+  res$setHeader('Access-Control-Allow-Origin', '*')
+  plumber::forward()
+}
+
+#* @preempt cors
 #* @get /sieve
 #* @post /sieve
 function(limit) sieveR::erastosthenesSieve(as.numeric(limit))
