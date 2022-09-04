26f3d8b73cefcde1b7d61e0c7845ee6c98e25e74
Fazendaaa <lucas.carotta@outlook.com>
2022-09-03 22:21:17 -0300

    Improving CORS to handle Chromium based request integration

--- inst/sieve.R
+++ inst/sieve.R
@@ -1,11 +1,17 @@
 # https://www.rplumber.io/articles/security.html#cross-origin-resource-sharing-cors
+# https://github.com/rstudio/plumber/issues/66#issuecomment-418660334
 #* @filter cors
-cors <- function(res) {
+cors <- function(req, res) {
   res$setHeader('Access-Control-Allow-Origin', '*')
-  res$setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
-  res$setHeader('Access-Control-Allow-Headers', 'append,delete,entries,foreach,get,has,keys,set,values,Authorization')
 
-  plumber::forward()
+  if (req$REQUEST_METHOD == 'OPTIONS') {
+    res$setHeader('Access-Control-Allow-Methods','*')
+    res$setHeader('Access-Control-Allow-Headers', req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
 }
 
 #* @get /sieve
